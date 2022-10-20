f486fbf6031d3c85af95515b11cef475109866fb
Md. Riyadul Hasan <riadulhasancse48@gmail.com>
2022-10-20 15:43:17 +0600

    Create dict_practice_1.py

---
+++ dict_practice_1.py
@@ -0,0 +1,10 @@
+first_dict = {
+    "name": "freeCodeCamp", 
+    "founder": "Quincy Larson",
+    "type": "charity", 
+    "age": 8, 
+    "price": "free", 
+    "work-style": "remote",
+}
+price1=first_dict.get('price')
+print(price1)
